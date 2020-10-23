12679c684d05ca37f4a70b7f9f724a9f0d531cb7
acejavelazquez <70553610+acejavelazquez@users.noreply.github.com>
2020-10-23 01:52:12 -0500

    Update settings.py
    Changed debug to false

--- MavBookBlog/settings.py
+++ MavBookBlog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h9@7m_v&lpjh^^vrj*w82y)mfz((ngxo0&jop2(6=kgw)wyn4c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
